395861a16248a04fec1af5548d8d254ba80da677
Christophe Gragnic <cgragnic@protonmail.com>
2017-06-20 20:36:23 +0200

    Use directly Teachers for the variables'domain

--- repart.py
+++ repart.py
@@ -11,6 +11,12 @@ class Teacher:
         self.maxi = maxi
         self.time = 0
 
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
 class Repart(constraint.Problem):
     """Problem about teachers dispatching"""
     
@@ -20,13 +26,9 @@ class Repart(constraint.Problem):
         super().__init__()
         self.teachersList = teachersList
 
-    def getTeacherByName(self, name):
-        for teacher in self.teachersList:
-            if teacher.name == name:
-                return teacher
 
     def addClass(self, name, time):
-        self.addVariable(name, [t.name for t in self.teachersList])
+        self.addVariable(name, self.teachersList)
         self.timeAmount[name] = time
 
     def prettySolutions(self):
@@ -35,7 +37,7 @@ class Repart(constraint.Problem):
             for t in self.teachersList:
                 t.time = 0
             for c, t in s.items():
-                self.getTeacherByName(t).time += self.timeAmount[c]
+                t.time += self.timeAmount[c]
             for t in self.teachersList:
                 print(t.name.ljust(3, ' '), t.time)
             print()
